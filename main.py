a0eb51ff9835a2657fae8b6e5913f3e3f32ef443
abdullah97 <abdullahalshawa@gmail.com>
2023-08-14 11:14:26 +0400

    Cleanup

--- main.py
+++ main.py
@@ -12,8 +12,6 @@ sentiment = sentiment_analyzer(text)
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-x = torch.empty(1)
-print(x)
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
