31c67d18294742dec132eb074f3ae8303f124032
abdullah97 <abdullahalshawa@gmail.com>
2023-08-16 10:09:11 -0700

    Submit name

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#Abdullah Mutaz Alshawa
 # This is a sample Python script.
 import torch
 from transformers import pipeline
@@ -13,18 +14,17 @@ sentiment = sentiment_analyzer(text)
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-def print_hi(name):
+def print_bye(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
+    print(f'Bye, {name}')  # Press ⌘F8 to toggle the breakpoint.
+print('Welcome')
 #Print sentiment and label
 for result in sentiment:
     label = result['label']
     score = result['score']
     print(f"Sentiment: {label}, Score: {score:.4f}")
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_bye('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
